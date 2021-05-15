82277388f6401bc84ddf7194144f31faa8888651
yonathan Admasu <admyoni4@yahoo.com>
2021-05-15 16:03:30 -0700

    worked on my login component and set it up with axioswithAuth successfully

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,60 @@
 import React, { useEffect } from "react";
+import { useState } from "react";
+import { axiosWithAuth } from '../helpers/axiosWithAuth';
+import { useHistory } from 'react-router-dom';
+
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [form,setForm] = useState({username: '', pasword: ''});
+  const [error, setError] = useState("")
+
+  const { push } = useHistory
 
   useEffect(()=>{
     // make a post request to retrieve a token from the api
     // when you have handled the token, navigate to the BubblePage route
+
   });
-  
-  const error = "";
+
+  //const error = "";
   //replace with error state
 
+
+  const handleChange = (e) => {
+    setForm({
+      ...form, 
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+    .post("/login", form)
+    .then((res) => {
+      console.log(res);
+      localStorage.setItem("token", res.data.payload);
+    })
+    .catch((err) => setError(err.response.data.error));
+  }
+  
+  
   return (
     <div>
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
-        <h2>Build login form here</h2>
+        <form onSubmit={handleSumbit}>
+          <label>
+            username:
+            <input name="username" onChange={handleChange} value={form.username} />
+          </label>
+          <label>
+            password:
+            <input name="password" onChange={handleChange} value={form.password} type="password" />
+          </label>
+          <button type="submit">Login</button>
+        </form>
       </div>
 
       <p data-testid="errorMessage" className="error">{error}</p>
